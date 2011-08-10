2e0a7d24ec2322d877090ba3c8cb2e00725fe2df
Simon Wex <simon@simonwex.com>
2011-08-09 17:38:13 -0700

    New app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IamwhoiAm::Application.config.secret_token = 'e17e5d16c38b9ed48701ee12cda45ef6008fa971c0f24dbfea1d76c6b8c5723a25fb2a328ec340d9ab7fb4841b9eda787a470363e0fb29719292a6099bcce834'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+IamwhoiAm::Application.config.session_store :cookie_store, :key => '_iamwhoi.am_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# IamwhoiAm::Application.config.session_store :active_record_store
